19956aea65f4be917c70414c85cf41200662d941
¨Dilber <dilberkrkt@mail.com>
2023-09-05 18:30:43 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int n,k;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayiyi giriniz(n) : ");
+        n = input.nextInt();
+        System.out.print("Ussu giriniz(k) : ");
+        k = input.nextInt();
+
+        int total = 1;
+        int i = 1;
+// 3^4 = 3 * 3 * 3 * 3
+        while (i<=k) {
+            total *= n ;
+            i++;
+        }
+        System.out.println("Cevap : " + total);
+    }
+}
